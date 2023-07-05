3f8ab6b4a4c3dc32e133dacb435e00c4959cff5c
Nadia Makeeva <lossa@inbox.ru>
2023-07-05 12:51:00 +0300

    update 1 file

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -9,14 +9,18 @@ public abstract class BaseSchema {
     List<Predicate> list = new ArrayList<>();
 
     /**
-     * Checks validity of the scheme
+     * Checks validity of the scheme.
+     * @param object
+     * @return scheme is valid or no
      */
     public boolean isValid(Object object) {
         return list.stream().allMatch(s -> s.test(object));
     }
 
     /**
-     * Adds a predicate to the predicates list
+     * Adds a predicate to the predicates list.
+     * @param predicate
+     * @return list of the predicates
      */
     public List<Predicate> addToList(Predicate predicate) {
         list.add(predicate);
